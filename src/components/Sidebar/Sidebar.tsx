0d66c7f66b20cc4a8ff417ad581b4b917e6483f6
khalifahnur <khalifahnur1095@gmail.com>
2024-12-23 19:13:49 +0300

    ref:sidebar ui

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import {
+  Home,
+  Ticket,
+  Calendar,
+  CreditCard,
+  Settings,
+  LogOut,
+  HelpCircle,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarRail,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+const sidebarItems = [
+  { name: "Home", href: "/home", icon: Home },
+  { name: "Tickets", href: "/tickets", icon: Ticket },
+  { name: "Matches", href: "/matches", icon: Calendar },
+  { name: "Membership", href: "/membership", icon: CreditCard },
+];
+
+const footerItems = [
+  { name: "Settings", href: "/settings", icon: Settings },
+  { name: "Logout", href: "#", icon: LogOut },
+  { name: "Need Help", href: "#", icon: HelpCircle },
+];
+
+export function AppSidebar() {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar variant="floating">
+      <SidebarHeader className="items-center justify-center">
+        <Link href="/home" className="flex-shrink-0">
+          <Image
+            src="https://www.murangaseal.com/assets/logo-a25ccce319b09f73006dc94d71887dbd26f5afeec59c2fa5dca6afaf101fe82c.png"
+            alt="Muranga Seals"
+            width={50}
+            height={50}
+            className="h-24 w-auto "
+          />
+        </Link>
+      </SidebarHeader>
+      <SidebarSeparator className="mt-2"/>
+      <SidebarContent>
+        <SidebarMenu>
+          {sidebarItems.map((item) => (
+            <SidebarMenuItem key={item.name}>
+              <SidebarMenuButton asChild isActive={pathname === item.href}>
+                <Link href={item.href}>
+                  <item.icon className="mr-2 h-4 w-4" />
+                  {item.name}
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarSeparator className="mt-2"/>
+      <SidebarFooter>
+        <SidebarMenu>
+          {footerItems.map((item) => (
+            <SidebarMenuItem key={item.name}>
+              <SidebarMenuButton asChild>
+                <Link href={item.href}>
+                  <item.icon className="mr-2 h-4 w-4" />
+                  {item.name}
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
