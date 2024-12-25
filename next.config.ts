146e14e94e8819d6c3306a03dd8b8f8492b395d5
khalifahnur <khalifahnur1095@gmail.com>
2024-12-25 17:28:39 +0300

    added:domain for sportpesa

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains: ['www.murangaseal.com','img.icons8.com'],
+    domains: ['www.murangaseal.com','img.icons8.com','www.ke.sportpesa.com'],
 }
 };
 
