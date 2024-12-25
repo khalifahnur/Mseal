3bcd4d8891f1e779bfc518a45e0f0aae07eedb3b
khalifahnur <khalifahnur1095@gmail.com>
2024-12-25 17:46:19 +0300

    feat:added membership benefits component

---
+++ src/components/pages/landing/membershipbenefits.tsx
@@ -0,0 +1,9 @@
+import { BenefitsPath } from "./benefitpath";
+
+export function MembershipBenefits() {
+  return (
+    <section className="py-12 bg-[#000] flex items-center justify-center">
+      <BenefitsPath />
+    </section>
+  );
+}
