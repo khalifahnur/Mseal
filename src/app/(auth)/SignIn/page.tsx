68e15513d212fcc98279bca472de26055981f4a1
khalifahnur <khalifahnur1095@gmail.com>
2024-12-25 17:33:52 +0300

    prettier:applied prettier

--- src/app/(auth)/SignIn/page.tsx
+++ src/app/(auth)/SignIn/page.tsx
@@ -1,10 +1,10 @@
-import LoginForm from '@/components/Forms/Signin/SigninForm'
-import React from 'react'
+import LoginForm from "@/components/Forms/Signin/SigninForm";
+import React from "react";
 
 export default function page() {
   return (
     <div>
       <LoginForm />
     </div>
-  )
+  );
 }
