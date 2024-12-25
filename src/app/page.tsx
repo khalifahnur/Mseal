e62436b185a8b030ed7c9358e05eb06ba9d405f6
khalifahnur <khalifahnur1095@gmail.com>
2024-12-25 17:35:28 +0300

    ref:added landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import MembershipSection from "@/components/pages/landing/Container";
+import { CTASection } from "@/components/pages/landing/cta";
+import { FAQSection } from "@/components/pages/landing/faqsection";
+import { Footer } from "@/components/pages/landing/Footer";
 import LandingHeader from "@/components/pages/landing/Header";
-import Image from "next/image";
+import { MembershipBenefits } from "@/components/pages/landing/membershipbenefits";
+import { PricingSection } from "@/components/pages/landing/pricesection";
+import { Testimonials } from "@/components/pages/landing/testimonial";
 
 export default function Home() {
   return (
-    <div className="h-full bg-white font-[family-name:var(--font-geist-sans)]">
+    <div className="h-full bg-[#f7f7f7] font-[family-name:var(--font-geist-sans)]">
       <LandingHeader />
       <MembershipSection />
+      <MembershipBenefits />
+      <PricingSection />
+      <CTASection />
+      <Testimonials />
+      <FAQSection />
+      <Footer />
     </div>
   );
 }
