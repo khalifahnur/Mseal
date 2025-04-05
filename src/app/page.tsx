4cf69fc8179fcf5e61999ef60da92b21a0ed506c
khalifahnur <khalifahnur1095@gmail.com>
2025-04-05 22:20:23 +0300

    style:changed component style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,23 @@ import { FAQSection } from "@/components/pages/landing/faqsection";
 import { Footer } from "@/components/pages/landing/Footer";
 import LandingHeader from "@/components/pages/landing/Header";
 import { MembershipBenefits } from "@/components/pages/landing/membershipbenefits";
-import { PricingSection } from "@/components/pages/landing/pricesection";
-import { Testimonials } from "@/components/pages/landing/testimonial";
+import Pricing from "@/components/pages/landing/pricesection";
+import Testimonials from "@/components/pages/landing/testimonial";
+import TrustedVenues from "@/components/pages/landing/trustedvenues";
 
 export default function Home() {
   return (
     <div className="h-full bg-[#f7f7f7] font-[family-name:var(--font-geist-sans)]">
       <LandingHeader />
+      <main className="overflow-x-hidden">
       <MembershipSection />
+      <TrustedVenues />
       <MembershipBenefits />
-      <PricingSection />
+      <Pricing />
       <CTASection />
       <Testimonials />
       <FAQSection />
+      </main>
       <Footer />
     </div>
   );
