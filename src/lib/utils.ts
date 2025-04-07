2f77418d7b8fb55ea568f47327eea2b01da154f5
khalifahnur <khalifahnur1095@gmail.com>
2025-04-07 19:31:06 +0300

    feat:formatdate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string): string {
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+
+  return new Date(dateString).toLocaleDateString("en-US", options)
+}
+
+
