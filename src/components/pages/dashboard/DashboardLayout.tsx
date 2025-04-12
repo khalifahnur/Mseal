5e98404f09f8775aa20e686b900b8a6518b7032a
khalifahnur <khalifahnur1095@gmail.com>
2025-04-12 21:44:03 +0300

    ref:fetched user info froma na api

--- src/components/pages/dashboard/DashboardLayout.tsx
+++ src/components/pages/dashboard/DashboardLayout.tsx
@@ -6,18 +6,24 @@ import { SidebarInset } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
+  const pathname = usePathname();
+
   const getCurrentTabName = () => {
-    const path = pathname.split('/')[1]
-    return path.charAt(0).toUpperCase() + path.slice(1) || 'Home'
-  }
+    const path = pathname.split("/")[1];
+    return path.charAt(0).toUpperCase() + path.slice(1) || "Home";
+  };
+
   return (
-    <div className="flex min-h-screen flex-col md:flex-row w-full ">
-      <AppSidebar />
-      <SidebarInset className="flex flex-1 flex-col">
-        <Header currentTab={getCurrentTabName()} />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-[#f8f8f8]">{children}</main>
-      </SidebarInset>
-    </div>
+    <>
+      <div className="flex min-h-screen flex-col md:flex-row w-full">
+        <AppSidebar />
+        <SidebarInset className="flex flex-1 flex-col">
+          <Header currentTab={getCurrentTabName()} />
+          <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-[#f8f8f8]">
+            {children}
+          </main>
+        </SidebarInset>
+      </div>
+    </>
   );
 }
