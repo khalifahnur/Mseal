49c23d5b360e78f5f60b8311770e480219ee872e
khalifahnur <khalifahnur1095@gmail.com>
2025-04-12 21:40:58 +0300

    ref:only allow access to authenticated users

--- src/app/(dash)/layout.tsx
+++ src/app/(dash)/layout.tsx
@@ -1,11 +1,20 @@
 import { DashboardLayout } from "@/components/pages/dashboard/DashboardLayout";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({ children }: { children: React.ReactNode }) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    redirect("/signin");
+  }
+
   return (
     <SidebarProvider>
-      <DashboardLayout>{children}</DashboardLayout>
+        <DashboardLayout>{children}</DashboardLayout>
     </SidebarProvider>
   );
 }
