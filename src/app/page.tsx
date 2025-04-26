7c9506c0e5e39297d47184249d9c74ea2750e9b6
khalifahnur <khalifahnur1095@gmail.com>
2025-04-26 10:33:44 +0300

    style:landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,6 @@
-"use client";
-
-import MembershipSection from "@/components/pages/landing/Container";
-import { CTASection } from "@/components/pages/landing/cta";
-import { FAQSection } from "@/components/pages/landing/faqsection";
-import { Footer } from "@/components/pages/landing/Footer";
-import LandingHeader from "@/components/pages/landing/Header";
-import { MembershipBenefits } from "@/components/pages/landing/membershipbenefits";
-import Pricing from "@/components/pages/landing/pricesection";
-import Testimonials from "@/components/pages/landing/testimonial";
-import TrustedSponsors from "@/components/pages/landing/TrustedSPonsor";
-
+import { AppWrapper } from "@/components/AppWrapper";
+import React from "react";
 
 export default function Home() {
-  return (
-    <div className="h-full bg-[#f7f7f7] font-[family-name:var(--font-geist-sans)]">
-      <LandingHeader />
-      <main className="overflow-x-hidden">
-      <MembershipSection />
-      <TrustedSponsors />
-      <MembershipBenefits />
-      <Pricing />
-      <CTASection />
-      <Testimonials />
-      <FAQSection />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <AppWrapper />
 }
