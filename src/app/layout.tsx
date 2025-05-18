32bdfaf044d16151202a6e21f94b443e7130aadf
khalifahnur <khalifahnur1095@gmail.com>
2025-05-18 09:14:29 +0300

    ref:created an external provider and let the main _layout to have server side component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,33 @@
-"use client";
-
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer} from 'react-toastify';
-import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Providers } from './provider';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: 'MSeal Membership',
   description: 'M-seal membership',
-}
-
-
-const queryClient = new QueryClient();
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-
+}) {
   return (
-    <html>
-      <QueryClientProvider client={queryClient}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <ToastContainer position="top-center" autoClose={3000} hideProgressBar />
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
-      </QueryClientProvider>
     </html>
   );
 }
-
